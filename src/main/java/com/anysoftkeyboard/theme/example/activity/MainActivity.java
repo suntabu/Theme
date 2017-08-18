28102d3335092952fc35461188fad866ddb13ff6
gouzhun <gouzhun@nexa-corp.com>
2017-08-18 10:15:44 +0800

    setup project

---
+++ src/main/java/com/anysoftkeyboard/theme/example/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.anysoftkeyboard.theme.example.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by gouzhun on 17/8/18.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
